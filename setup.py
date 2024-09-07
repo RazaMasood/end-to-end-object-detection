0351da79ec1697463b6f9a7fc9fd5e20c5a1a60f
RazaMasood <razamasood954@gmail.com>
2024-09-07 10:48:58 +0530

    data_ingestion added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Object Detection',
+    version= '0.0.0',
+    author= 'Mohd Masood Raza',
+    author_email= 'razamasood954@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
